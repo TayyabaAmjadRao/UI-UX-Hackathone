72301bd3b7a07da1986b961b1dd3f0101cd0d651
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-12-08 19:45:39 +0500

    Update AccountSidebar.tsx

--- src/components/account/AccountSidebar.tsx
+++ src/components/account/AccountSidebar.tsx
@@ -5,6 +5,7 @@ import { deleteAccount } from "../../redux/features/authSlice";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
+
 const MySwal = withReactContent(Swal)
 
 const AccountSidebar = () => {
