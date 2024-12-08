1ab1dd5221d91b4484f39231bcff5a151fc110ef
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-12-08 20:06:59 +0500

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,6 +2,7 @@ import logo from '../assets/images/logo.svg';
 import { FeaturesBar, SecondaryHero, Reveal } from "../components/common/index";
 import ContactForm from "../components/contact/ContactForm";
 
+
 const Contact = () => {
   return (
     <>
